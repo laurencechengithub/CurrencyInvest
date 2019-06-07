4a0dd065106c8885d321e0fe06d73e16530795b0
Laurence Chen Mini <laurence.email@gmail.com>
2019-06-07 11:22:50 +0800

    [clear] some code

--- CurrencyInvest/AppDelegate.swift
+++ CurrencyInvest/AppDelegate.swift
@@ -116,13 +116,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
             
         }
-        print("applicationWillEnterForeground")
+        dPrint("applicationWillEnterForeground")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         
-        print(applicationDidBecomeActive)
+        dPrint(applicationDidBecomeActive)
         
         guard let shortCutItem = launchedShortcutItem else {
             return
